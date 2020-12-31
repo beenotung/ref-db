21f4964a8dba8e7170db086feefbbc40eeb8fe76
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-12-31 08:52:40 +0800

    lint fix

--- src/utils/collection.ts
+++ src/utils/collection.ts
@@ -4,12 +4,9 @@ export function toCollection<T extends Record>(
   collection: T[] | { [id: string]: T },
 ): { [id: string]: T } {
   return Array.isArray(collection)
-    ? collection.reduce(
-        (acc, c) => {
-          acc[c.id] = c;
-          return acc;
-        },
-        {} as { [id: string]: T },
-      )
+    ? collection.reduce((acc, c) => {
+        acc[c.id] = c;
+        return acc;
+      }, {} as { [id: string]: T })
     : collection;
 }
